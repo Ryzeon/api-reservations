9cf566a6ef80b3912b8819faa8205644a312cfe2
Alex Avila <alex.ramon2004@gmail.com>
2024-07-24 20:26:49 -0500

    feat: reservation repository.

---
+++ src/main/java/me/ryzeon/reservations/repository/ReservationRepository.java
@@ -0,0 +1,9 @@
+package me.ryzeon.reservations.repository;
+
+import me.ryzeon.reservations.model.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+}
